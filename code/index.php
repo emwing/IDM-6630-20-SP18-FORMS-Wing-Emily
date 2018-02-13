a0561d21a9ef895c9c27179f43a1074c9385ce7b
emwing <emwing@emily-wing.com>
2018-02-13 00:40:49 -0500

    initial commit
    Check-In 5.1 clean repo

---
+++ code/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+
+    <meta name="author" content="Emily Wing">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/css.css">
+
+  </head>
+  <body>
+
+    <footer>
+      <p>&copy;2018 Emily Wing</p>
+    </footer>
+
+  </body>
+</html>
